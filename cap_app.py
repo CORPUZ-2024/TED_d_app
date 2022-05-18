f6583c5000d75c048b6055c64c0b05639db03ed4
NC <corpuzn12@gmail.com>
2022-05-17 17:41:57 -0700

    Update cap_app.py

--- cap_app.py
+++ cap_app.py
@@ -9,7 +9,7 @@ import plotly.express as px
 
 from PIL import Image
 
-
+import os
 
 
 st.title('Effects of Medicaid  Expansion to Treatment Completions in SUD Outpatient Programs')
@@ -88,7 +88,11 @@ In this section, we explore some of the features to get more insights on the tre
 in 2009 at 43.5% which steadily declined in the following years. Furthermore, the spread for each year tended to be normal in distribution among the states. 
 
 """
-dis_rate= pd.read_csv("dis_rate_agg.csv")
+
+
+
+
+dis_rate= pd.read_csv("./dis_rate_agg.csv")
 
 fig_dis2 = px.box(dis_rate, x="year", y="tmp_rate",notched=True,  hover_data=["state"], color_discrete_sequence=[ "#e884d6"])
 fig_dis2.update_traces(quartilemethod="exclusive") # or "inclusive", or "linear" by default
