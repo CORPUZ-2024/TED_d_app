a5fe7d237bfb5b86076fa9a407d308063b1762ea
NC <corpuzn12@gmail.com>
2022-05-17 22:21:50 -0700

    Update cap_app.py

--- cap_app.py
+++ cap_app.py
@@ -90,7 +90,7 @@ in 2009 at 43.5% which steadily declined in the following years. Furthermore, th
 """
 #https://github.com/corpuzn12/TED_d_app/blob/main/Datasets/dis_rate_agg.csv
 
-dis_rate= pd.read_csv('main/Datasets/dis_rate_agg.csv')
+dis_rate= pd.read_csv('../Datasets/dis_rate_agg.csv')
 
 fig_dis2 = px.box(dis_rate, x="year", y="tmp_rate",notched=True,  hover_data=["state"], color_discrete_sequence=[ "#e884d6"])
 fig_dis2.update_traces(quartilemethod="exclusive") # or "inclusive", or "linear" by default
