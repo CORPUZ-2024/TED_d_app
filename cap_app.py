174fc16c802f9bd05da1914b5a400dca89c005a3
NC <corpuzn12@gmail.com>
2022-05-17 19:34:45 -0700

    Update cap_app.py

--- cap_app.py
+++ cap_app.py
@@ -88,9 +88,10 @@ In this section, we explore some of the features to get more insights on the tre
 in 2009 at 43.5% which steadily declined in the following years. Furthermore, the spread for each year tended to be normal in distribution among the states. 
 
 """
-dis_rate = './disrate_annu_agg.csv'
 
-dis_rate= pd.read_csv(dis_rate)
+dis_rate = open('./disrate_annu_agg.csv')
+
+#dis_rate= pd.read_csv(dis_rate)
 
 fig_dis2 = px.box(dis_rate, x="year", y="tmp_rate",notched=True,  hover_data=["state"], color_discrete_sequence=[ "#e884d6"])
 fig_dis2.update_traces(quartilemethod="exclusive") # or "inclusive", or "linear" by default
